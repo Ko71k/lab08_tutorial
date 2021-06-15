36ce4830d1389da15d083a2fd6168fceb985d889
ko71k <rassmagin.wolf@mail.ru>
2021-06-15 12:56:18 +0300

    all

---
+++ _builds/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/tests/test1.cpp" "/home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/_builds/CMakeFiles/check.dir/tests/test1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/ko71k/projects/hunter/_Base/xxxxxxx/252be92/551f543/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/_builds/CMakeFiles/print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial
+# Build directory: /home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/CMakeLists.txt;66;add_test;/home/ko71k/Desktop/ko71k/workspace/projects/lab7_tutorial/CMakeLists.txt;0;")
